fddfd2b8205ba86ceab82ba478adbfafc1470e1f
Nick Bourque <nick@binogi.com>
2022-02-22 14:04:05 -0400

    attempt 3 at new avatar

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 window.onload = e => {
     let avatar = document.createElement('img')
     avatar.className = 'header-avatar'
-    avatar.src = '../img/nick-head.png'
+    avatar.src = 'assets/img/nick-head.png'
     avatar.alt = 'Nick Bourque'
 
     document.querySelector('header .container').append(avatar)
